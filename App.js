812aa4c3a2e752a81ae349005935a5d6b1458d58
swetanagur <swetanagur@gmail.com>
2025-09-25 13:19:36 -0400

    commiting the new code

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
-const heading = React.createElement('h1',{}, 'Hello world from React!');
+const heading = React.createElement('h1',{id:"heading",xyz:'xyz'}, 'Hello world from React!');
+console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(heading);
